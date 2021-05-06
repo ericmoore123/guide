b85f65e81a3e1cad542eecd55694a986c62d59d7
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-05-06 19:42:16 -0400

    Lists

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import pokemon from './pokemon.json';
 
 function App() {
   return (
     <div style={{margin: "auto", width: "800px", paddingTop:"1rem"}}>
       <h1 className="title">Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0, 20).map((pokemon) => ( //slice incoming array to first 20 entries, then map over and format each item recieved
+            <tr key={pokemon.id}>
+              <td>{pokemon.name.english}</td>
+              <td>{pokemon.type.join(", ")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
