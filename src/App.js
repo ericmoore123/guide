8995bb5a268db9494cc61813b5107cb784dc73e0
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-05-10 18:52:16 -0400

    inserted state management, added functionality

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import pokemon from './pokemon.json';
 import PropTypes from "prop-types";
 
-const PokemonRow = ({pokemon}) => 
+const PokemonRow = ({pokemon, onSelect}) => 
   (
     <tr>
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
+      <td>
+        <button onClick={() => onSelect(pokemon)}>Select</button>
+      </td>
     </tr>
   );
 
+  // Proptypes Validate the prop passed to a child and warn the developer when a wrong props is passed, which may break your app.
 PokemonRow.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.shape({
@@ -18,28 +22,52 @@ PokemonRow.propTypes = {
     }),
     type: PropTypes.arrayOf(PropTypes.string),
   }),
+  onSelect: PropTypes.func,
 }
 
-function App() {
+const App = () => {
+
+  // React Hooks let you use state and other React features without writing a class based component
+  const [filter, filterSet] = React.useState(""); //UseState returns an array of two items (filter and filterset)
+  const [selectedItem, selectedItemSet] = React.useState(null);
+
   return (
-    <div style={{margin: "auto", width: "800px", paddingTop:"1rem"}}>
+    <div className="container" >
       <h1 className="title">Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
+      
+      <div className="table-style">
+        <div>
+          <input value={filter} onChange={(event) => filterSet(event.target.value)}></input>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
 
-        <tbody>
-          {pokemon.slice(0, 20).map((pokemon) =>  //slice incoming array to first 20 entries, then map over and format each item recieved
-            ( 
-              <PokemonRow pokemon={pokemon} key={pokemon.id} />
-            ))}
-        </tbody>
+            <tbody>
+              {pokemon
+              //Returns new array made only of items that include the inputted value in the input field 
+              //Add toLowerCase to make non-case-sensitive
+              .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+              // Returns only first 20 array values
+              .slice(0, 20)
+              // Create PokemonRow component instance for each item in (now) formatted array
+              .map((pokemon) =>  //slice incoming array to first 20 entries, then map over and format each item recieved
+                ( 
+                  <PokemonRow pokemon={pokemon} key={pokemon.id} onSelect={(pokemon) => selectedItemSet(pokemon)} />
+                ))}
+            </tbody>
 
-      </table>
+          </table>
+        </div>
+
+        {selectedItem && (
+          <h1>{selectedItem.name.english}</h1>
+        )}
+
+      </div>
     </div>
   );
 }
